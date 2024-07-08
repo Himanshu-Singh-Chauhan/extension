5e926091f9141a2963cfa250f64dd53edd05af9f
himanshu <him.speedo@gmail.com>
2024-07-09 00:04:53 +0530

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Button } from "@/shadcn/ui/button";
+import { Alert, AlertDescription, AlertTitle } from "@/shadcn/ui/alert";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/shadcn/ui/alert-dialog";
 
 function App() {
-
   return (
     <>
       <div className="text-3xl font-bold underline">
         Initial Extension setup.
+        <Button>Click me</Button>
+        <Alert>
+          {/* <Terminal className="h-4 w-4" /> */}
+          <AlertTitle>Heads up!</AlertTitle>
+          <AlertDescription>
+            You can add components and dependencies to your app using the cli.
+          </AlertDescription>
+        </Alert>
+        <AlertDialog>
+          <AlertDialogTrigger>Open</AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
